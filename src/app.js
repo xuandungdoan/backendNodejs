a4310d8e9262d1d5605846a6a0c861cb7017de92
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-02 20:57:57 +0700

    views

--- src/app.js
+++ src/app.js
@@ -6,10 +6,21 @@ const User = require('./User');
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.static('public'));
+
+app.get('/signin', (req, res) => res.render('signIn'));
+app.get('/signup', (req, res) => res.render('signUp'));
+
+app.post('/signin', parser, (req, res) => {
+
+});
+
+app.post('/signup', parser, (req, res) => {
+    
+});
 
 app.post('/user', parser, async (req, res) => {
     const user = new User(req.body);
     await user.save();
     res.send({ message: 'OK' });
 });
+module.exports = app;
