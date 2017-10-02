684d8ec30efefad772da6029705dabca7fd8eee9
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-02 19:54:42 +0700

    damuave

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+
+app.use(session({
+    secret: 'alsdh93e9d927d',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/muave', (req, res) => {
+    req.session.daMuaVe = true; // eslint-disable-line
+    res.send('Ban da mua ve');
+});
+
+app.get('/vaorap', (req, res) => {
+    if (req.session.daMuaVe) return res.send('Moi xem phim');
+    return res.send('Ban phai mua ve');
+});
+
+app.listen(3000, () => console.log('Server started!'));
